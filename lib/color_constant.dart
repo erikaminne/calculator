53fbf115d82ac342b30fb49bb398c936dcd328a8
erikaminne <89967266+erikaminne@users.noreply.github.com>
2024-04-25 17:56:28 +0300

    adding lib

---
+++ lib/color_constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static const Color textColor = Color.fromRGBO(255, 255, 255, 1);
+  static const Color mainButton = Color.fromRGBO(0, 133, 156, 1);
+  static const Color secondaryButton = Color.fromRGBO(44, 226, 8, 1);
+  static const Color equalButton = Color.fromRGBO(5, 255, 138, 1);
+  static const Color backgroundColor = Color.fromRGBO(32, 32, 32, 1);
+}
